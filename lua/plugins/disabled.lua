e27ea36a3936b0354acf4bb7e46f0168ca7f6be5
Prashanth Sadasivan <prashanth@limitless.ai>
2025-06-29 11:26:07 -0400

    trying kickstart - going to try lazyvim again

--- lua/plugins/disabled.lua
+++
@@ -1,5 +0,0 @@
-return {
-  -- disable community snippets
-  { "rafamadriz/friendly-snippets", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
-}
--- lua/plugins/ghcopilot.lua
+++
@@ -1,5 +0,0 @@
-vim.g.copilot_no_tab_map = true
-vim.notify("loading copilot")
-return {
-  "github/copilot.vim",
-}
