9ea13b0e3884de5dc1e9f19fe066b8b678189b2d
Prashanth Sadasivan <prashanth@limitless.ai>
2025-07-25 10:20:40 -0700

    vscode

--- init.lua
+++ init.lua
@@ -1,2 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+  vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "References", nowait = true })
+  vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "Goto Implementation" })
+  vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, { desc = "Goto T[y]pe Definition" })
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
+  vim.keymap.set("n", "K", function()
+    return vim.lsp.buf.hover()
+  end, { desc = "Hover" })
+  vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename File" })
+  vim.keymap.set("n", "<leader>cR", function()
+    Snacks.rename.rename_file()
+  end, { desc = "Rename File" })
+else
+  require("config.lazy")
+end
