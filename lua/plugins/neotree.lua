7631d4abcf2569a9bb41cc1e5057ab5241d92472
Prashanth Sadasivan <prashanth@limitless.ai>
2025-09-21 12:01:18 -0400

    updates

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,16 +1,16 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    cmd = "Neotree",
-    keys = {
-      {
-        "<Bslash>",
-        function()
-          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
-        end,
-        desc = "Explorer NeoTree (cwd)",
-      },
-    },
-  },
+  --{
+  --  "nvim-neo-tree/neo-tree.nvim",
+  --  branch = "v3.x",
+  --  cmd = "Neotree",
+  --  keys = {
+  --    {
+  --      "<Bslash>",
+  --      function()
+  --        require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+  --      end,
+  --      desc = "Explorer NeoTree (cwd)",
+  --    },
+  --  },
+  --},
 }
